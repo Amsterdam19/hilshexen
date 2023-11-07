38e3200a199a82b262a36a5c15a4713f0a4b281e
Amsterdam19 <150171356+Amsterdam19@users.noreply.github.com>
2023-11-07 17:55:59 +0100

    Add files via upload

---
+++ src/components/Datetime.tsx
@@ -0,0 +1,49 @@
+import { LOCALE } from "@config";
+
+export interface Props {
+  datetime: string | Date;
+  size?: "sm" | "lg";
+  className?: string;
+}
+
+export default function Datetime({ datetime, size = "sm", className }: Props) {
+  return (
+    <div className={`flex items-center space-x-2 opacity-80 italic ${className}`}>
+      <span className="sr-only">Veröffentlicht am:</span>
+      <span className="day">
+        <FormattedDay datetime={datetime} />.
+      </span>
+      <span>
+        <FormattedDatetime datetime={datetime} />
+      </span>
+    </div>
+  );
+}
+
+const FormattedDatetime = ({ datetime }: { datetime: string | Date }) => {
+  const myDatetime = new Date(datetime);
+
+  const date = myDatetime.toLocaleDateString(LOCALE, {
+    year: "numeric",
+    month: "long",
+  });
+
+  return (
+    <>
+      {date}
+    </>
+  );
+};
+const FormattedDay = ({ datetime }: { datetime: string | Date }) => {
+  const myDatetime = new Date(datetime);
+
+  const date = myDatetime.toLocaleDateString(LOCALE, {
+    day: "numeric",
+  });
+
+  return (
+    <>
+      {date}
+    </>
+  );
+};
