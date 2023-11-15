c2f5e23d2b499bac731ced0fc122312e6a54d5a6
Amsterdam19 <150171356+Amsterdam19@users.noreply.github.com>
2023-11-15 18:42:26 +0100

    img optimasation expanded

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -19,7 +19,7 @@ export default function Card({ href, frontmatter, secHeading = true, Card = true
 
   return (
     <a href={href} className={Card ? ("postCard card"): ("postCard")}>
-      <img className="h-full object-cover postImg" src={heroImage.src} alt={"Bild zum Artikel " + title}/>
+      <img className="h-full object-cover bg-cover postImg" src={heroImage.src} alt={"Bild zum Artikel " + title}/>
       <Datetime datetime={pubDatetime} className="text-skin-accent postDate mx-[5%]"/>
       <span
         className="inline-block font-medium decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0 text-balance text-3xl postTitle mx-[5%]"
