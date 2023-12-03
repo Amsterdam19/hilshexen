1281644f2de614f500d9daef6804dd707411a829
Amsterdam19 <150171356+Amsterdam19@users.noreply.github.com>
2023-12-03 18:54:59 +0100

    update und firefox fix

---
+++ dist/_astro/hoisted.0054eb69.js
@@ -0,0 +1,1 @@
+import"./hoisted.c3a4cedb.js";import"./index.cc9059a8.js";window.netlifyIdentity&&window.netlifyIdentity.on("init",i=>{i||window.netlifyIdentity.on("login",()=>{document.location.href="/admin/"})});
--- dist/_astro/hoisted.944770f3.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.b158c2de.js";import"./index.5ab1dcfa.js";window.netlifyIdentity&&window.netlifyIdentity.on("init",i=>{i||window.netlifyIdentity.on("login",()=>{document.location.href="/admin/"})});
