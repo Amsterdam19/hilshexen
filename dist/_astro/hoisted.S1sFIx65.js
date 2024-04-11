07661779a37cc96b6756c133a0ebf7dfd4da27b4
Amsterdam19 <150171356+Amsterdam19@users.noreply.github.com>
2024-04-11 12:59:20 +0200

    new ToC

--- dist/_astro/hoisted.S1sFIx65.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.u_KXsv1d.js";import"./index.hLBgGYxu.js";window.netlifyIdentity&&window.netlifyIdentity.on("init",i=>{i||window.netlifyIdentity.on("login",()=>{document.location.href="/admin/"})});
